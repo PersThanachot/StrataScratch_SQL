06a5a24292bc7540eec20ae9c4c7d315519ade20
Pers Thanachot <78893367+PersThanachot@users.noreply.github.com>
2022-02-05 16:12:02 +0700

    Create Counting Instances in Text.sql

---
+++ Counting Instances in Text.sql
@@ -0,0 +1,16 @@
+
+--Find the number of times the words 'bull' and 'bear' occur in the contents. We're counting 
+--the number of times the words occur so words like 'bullish' should not be included in our 
+--count.
+--Output the word 'bull' and 'bear' along with the corresponding number of occurrences.
+
+--------------------------------------------------------------------------------------------
+
+SELECT 
+    word,
+    nentry AS count_word                               
+FROM  
+    ts_stat('SELECT to_tsvector(contents) FROM google_file_store')
+WHERE 
+    word ILIKE 'bull' or
+    word ILIKE 'bear'
